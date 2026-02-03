afcecab8abc0169b1867447bcf2155fdce3a8575
DStacyy <gabutajajirlah@gmail.com>
2026-02-03 10:36:54 +0700

    beginilah

---
+++ func.php
@@ -0,0 +1,16 @@
+<?php
+function getBarang($keyword='', $kategori=''){
+    global $conn; 
+
+    $query = "SELECT * FROM barang WHERE 1=1";
+    $params = array();
+
+    //cek jika keyword ada nilainya, maka..
+    if(!empry ($keyword)){
+        $query .=" AND (nama_barang LIKE ? OR deskripsi LIKE ?)";
+        $params = "%$keyword%";
+        $params = "%$keyword%";
+
+    }
+}
+?>
